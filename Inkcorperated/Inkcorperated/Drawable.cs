3d5dc8dceb10ff8e0b6cc73e0a69d3971c29912e
jbb7824 <jbb7824@IGMOH1380-22>
2019-02-15 15:09:32 -0500

    Wrote the drawable and entity base classes and made the files for everything else

---
+++ Inkcorperated/Inkcorperated/Drawable.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Inkcorperated
+{
+    class Drawable
+    {
+        private Rectangle bounds;
+        private Texture2D texture;
+
+        public Rectangle Bounds
+        {
+            get
+            {
+                return bounds;
+            }
+        }
+
+        public int X
+        {
+            get
+            {
+                return bounds.X;
+            }
+
+            set
+            {
+                bounds.X = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return bounds.Y;
+            }
+
+            set
+            {
+                bounds.Y = value;
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return bounds.Width;
+            }
+
+            set
+            {
+                bounds.Width = value;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return bounds.Height;
+            }
+
+            set
+            {
+                bounds.Height = value;
+            }
+        }
+
+        public Drawable(Rectangle bounds, Texture2D texture)
+        {
+            this.bounds = bounds;
+            this.texture = texture;
+        }
+
+        /// <summary>
+        /// Draws this object to the spritebatch with the specified color
+        /// </summary>
+        public void Draw(SpriteBatch batch, Color c)
+        {
+            batch.Draw(texture, bounds, c);
+        }
+    }
+}
---
+++ Inkcorperated/Inkcorperated/Entity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Inkcorperated
+{
+    class Entity : Drawable
+    {
+        private float fireRate;
+        private double timeSinceLastShot;
+
+        public Entity(Rectangle bounds, Texture2D texture, float fireRate) : base(bounds, texture)
+        {
+            this.fireRate = fireRate;
+            timeSinceLastShot = 0;
+        }
+
+        /// <summary>
+        /// To be called every frame, updates the time since last shot
+        /// </summary>
+        public void Update(double timeSinceLastFrame)
+        {
+            timeSinceLastShot += timeSinceLastFrame;
+        }
+
+        /// <summary>
+        /// Checks to see if the entity can fire
+        /// Resets the timeSinceLast fire if so
+        /// </summary>
+        /// <returns>If the entity can fire</returns>
+        public bool Fire()
+        {
+            if(fireRate < timeSinceLastShot)
+            {
+                timeSinceLastShot = 0;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Collided(Drawable other)
+        {
+            return Bounds.Intersects(other.Bounds);
+        }
+    }
+}
