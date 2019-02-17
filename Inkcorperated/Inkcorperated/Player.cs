fa8be91a62d903bc428071a7a989d59e06804ef4
Rhys Stever <38188371+rhysstever@users.noreply.github.com>
2019-02-16 20:56:05 -0500

    Added inkLevel Field to Player Class, as well as the syntax for the Move method

--- Inkcorperated/Inkcorperated/Player.cs
+++ Inkcorperated/Inkcorperated/Player.cs
@@ -10,6 +10,27 @@ namespace Inkcorperated
 {
     class Player : Entity
     {
-        public Player(Rectangle bounds, Texture2D texture, float fireRate = 1.0f) : base(bounds, texture, fireRate) { }
+		// Fields
+
+		private int inkLevels; 
+		
+		// Constructor
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="bounds">The hitbox of the player</param>
+		/// <param name="texture">The visual of the player</param>
+		/// <param name="fireRate">How often the player is allowed to shoot</param>
+		public Player(Rectangle bounds, Texture2D texture, float fireRate = 1.0f) : base(bounds, texture, fireRate)
+		{
+			inkLevels = 100; // starting value of ink (can be changed for balancing)
+		}
+
+		// Methods
+
+		public void Move(Block floor)
+		{
+
+		}
     }
 }
